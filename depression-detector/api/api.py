3302ac3e195eda479d9ae06a1894877f1866a06e
Ava Robinson <avamrobinson@gmail.com>
2020-12-08 11:43:36 -0800

    following tutorial 1

--- depression-detector/api/api.py
+++ depression-detector/api/api.py
@@ -16,9 +16,19 @@ import re
 import time
 import itertools
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 # CORS(app)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/api/v1', methods = ['POST'])
 def scrape_and_predict():
